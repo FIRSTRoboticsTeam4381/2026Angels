3363b76e5ccb91c1c5213752317f32678c6d0ebe
Ella <Ella@leathermans.net>
2026-01-27 20:29:24 -0500

    added AutoAim stuff needs mroe

--- src/main/java/frc/robot/AutoAim.java
+++ src/main/java/frc/robot/AutoAim.java
@@ -3,6 +3,60 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-
+//hi people 
 /** Add your docs here. */
-public class AutoAim {}
+
+import static edu.wpi.first.units.Units.Meter;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.DistanceUnit;
+import edu.wpi.first.units.Unit;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import frc.lib.commands.SwerveAngle;
+
+public class AutoAim {
+
+public static Command autoAimSwerve(Supplier<Double> forward, Supplier<Double> leftright)
+{
+    return new ConditionalCommand
+    (() -> isRed(),
+      new ConditionalCommand(() -> redAllianceZone(),
+         null, 
+         new SwerveAngle(RobotContainer.getRobot().swerve,
+          forward, leftright, () -> Rotation2d.kCCW_90deg)), 
+      new ConditionalCommand(() -> blueAllianceZone(),
+         null, 
+         new SwerveAngle(RobotContainer.getRobot().swerve,
+          forward, leftright, () -> Rotation2d.kCW_90deg)))
+          //change later for the rotation2d.KCW_90deg to rotation2d.KCCW_90deg
+          //KCCW is counterclockwise KCW is clockwise
+}
+
+public static boolean isRed()
+{
+   Optional<Alliance> a= DriverStation.getAlliance();
+
+   if(a.isEmpty())
+    return true;
+
+    else
+        return a.get() == Alliance.Red;
+}
+ 
+    public static boolean redAllianceZone()
+    {
+        return RobotContainer.getRobot().swerve.swerveOdometry.getEstimatedPosition().getMeasureX().in(Meter) > 12;
+    }
+
+    public static boolean blueAllianceZone()
+    {
+        return RobotContainer.getRobot().swerve.swerveOdometry.getEstimatedPosition().getMeasureX().in(Meter) < 4.5;
+    }
+}

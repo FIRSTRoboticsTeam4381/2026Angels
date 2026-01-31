c75d12e1c52465a3061e353b09db14c9c1260e04
nmwal <nmwalton30@gmail.com>
2026-01-31 14:00:11 -0500

    Set velocity in the shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,6 +11,7 @@ import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -64,9 +65,32 @@ public SparkFlex shooter2;
       //gives 0.5 current to the motor 
     }
 
-    private void setVelocity(double v)
+    private void setVelocity(double velocity)
     {
-        shooter1.getClosedLoopController().setSetpoint(v, ControlType.kVelocity);
+        shooter1.getClosedLoopController().setSetpoint(velocity, ControlType.kVelocity);
         // keeps the speed around the same speed 
     }
+
+    
+
+     public InterpolatingDoubleTreeMap table = new InterpolatingDoubleTreeMap();
+
+      public void setUp()
+        {
+          table.put(0.0, 0.0);
+          table.put(1.0, 10.0);
+          table.put(2.0, 30.0);
+        }
+
+      public Command velocityFromDistance(double distance)
+        {
+          double result =table.get(distance);
+          return new InstantCommand(() -> setVelocity(result));
+        }
+
+
+
+
+
+
 }
